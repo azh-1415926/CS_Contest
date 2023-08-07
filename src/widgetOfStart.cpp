6b6a60ca66aa19e02b4047bc12bcc9a61e766d0d
azh <azhandhj@outlook.com>
2023-08-07 11:14:28 +0800

    粗略绘制答题界面

--- src/widgetOfStart.cpp
+++ src/widgetOfStart.cpp
@@ -5,6 +5,23 @@ widgetOfStart::widgetOfStart(QWidget *parent)
     , ui(new Ui_widgetOfStart)
 {
     ui->setupUi(this);
+    QStackedWidget* windows=this->ui->stackOfWindows;
+    QPushButton* buttons[]={
+        ui->buttonOfQuestion,
+        ui->buttonOfSelect,
+        ui->buttonOfCollect
+    };
+    QWidget* pages[]={
+        ui->pageOfQuestion,
+        ui->pageOfSelect,
+        ui->pageOfCollect
+    };
+    for(int i=0;i<sizeof(buttons)/sizeof(QPushButton*);i++){
+        connect(buttons[i],QPushButton::clicked,this,[windows,buttons,pages,i](){
+            windows->setCurrentIndex(windows->indexOf(pages[i]));
+        });
+    }
+    
 }
 
 widgetOfStart::~widgetOfStart()
