c4f2973da01b47e141aaeb8db3cd710e777fff27
azh <azhandhj@outlook.com>
2023-08-17 23:38:39 +0800

    更新 collectButton，即收藏按钮组件的注释

--- component/collectbutton.cpp
+++ component/collectbutton.cpp
@@ -13,11 +13,13 @@ collectButton::~collectButton()
 
 }
 
+/* 返回是否被收藏，被收藏返回 true */
 bool collectButton::isCollect()
 {
     return this->text()==textOfCollect;
 }
 
+/* 重写鼠标点击事件，每点击一次，都会触发收藏或取消收藏，并发送 collected、uncollected 信号 */
 void collectButton::mousePressEvent(QMouseEvent *e)
 {
     if(this->text()==textOfCollect){
@@ -29,23 +31,27 @@ void collectButton::mousePressEvent(QMouseEvent *e)
     }
 }
 
+/* 设置按钮状态为被收藏，发送 collected 信号 */
 void collectButton::setCollect()
 {
     this->setText(textOfCollect);
     emit collected();
 }
 
+/* 设置按钮状态为取消收藏，发送 uncollected 信号 */
 void collectButton::setUncollect()
 {
     this->setText(textOfUncollect);
     emit uncollected();
 }
 
+/* 设置按钮状态为被收藏，不发送信号 */
 void collectButton::setCollectNoSignal()
 {
     this->setText(textOfCollect);
 }
 
+/* 设置按钮状态为取消收藏，不发送信号 */
 void collectButton::setUncollectNoSignal()
 {
     this->setText(textOfUncollect);
